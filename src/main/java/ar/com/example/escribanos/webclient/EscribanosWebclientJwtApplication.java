1d4feaa77692881c8ec13cc5c4e3e3b9962e54b6
Ariel Rivero <jrivero@hipotecario.com.ar>
2025-04-22 20:57:53 -0300

    First commit

---
+++ src/main/java/ar/com/example/escribanos/webclient/EscribanosWebclientJwtApplication.java
@@ -0,0 +1,13 @@
+package ar.com.example.escribanos.webclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EscribanosWebclientJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EscribanosWebclientJwtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ar/com/example/escribanos/webclient/EscribanosWebclientJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.com.example.escribanos.webclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EscribanosWebclientJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
